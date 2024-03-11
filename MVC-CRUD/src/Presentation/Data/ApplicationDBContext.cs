12f024963f82d21e86e21a3f4bf37ad9ca579d48
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-03-11 14:17:47 +0500

    Add project files.

---
+++ MVC-CRUD/src/Presentation/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Presentation.Models.Entities;
+
+namespace Presentation.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
